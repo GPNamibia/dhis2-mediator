b45fc3de417d19e12a17926d7dc6eb7cab3fa707
Mirjam Kayavala <mirjamkayavala@gmail.com>
2022-09-21 18:38:40 +0200

    NMA-618(Saving data from DHIS2 and registering the dat a to DHIS2 and dockerisng the data)

---
+++ src/models/fact_anc_dhis2_export.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, DataTypes) => {
+    const fact_anc_dhis2_export = sequelize.define("fact_anc_dhis2_export", {
+      data_element: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      period: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      org_unit_uid: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      COCUID: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      AOCUID: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      datavalue: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    });
+    return fact_anc_dhis2_export;
+  };
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
---
+++ src/models/log_table.js
@@ -0,0 +1,58 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const log_tables = sequelize.define("log_tables", {
+        _id: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+              notEmpty: true,
+            },
+          },
+      date: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      response_text: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      response_status: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      last_resend_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      details: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate:{
+            notEmpty: true,
+        },
+
+      },
+    });
+    return log_tables;
+  };
+  
+  
