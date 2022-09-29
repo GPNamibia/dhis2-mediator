61fa1bc8853594787e0dc01fb0b33a37fc1043d4
Mirjam Kayavala <mirjamkayavala@gmail.com>
2022-09-29 08:44:26 +0200

    NMA-618, implemented all the changes from the feedback

--- test/dummyData/response.js
+++ test/dummyData/response.js
@@ -1,46 +1,45 @@
 const rawData = [
-    {
-      data_element: 'UYvwaXzgmP9',
-      period: '201610',
-      org_unit_uid: 'LIXOLUllVKI',
-      COCUID: '',
-      AOCUID: '',
-      datavalue: ''
-    },
-    {
-      data_element: 'gHGIS9Mo9te',
-      period: '201705',
-      org_unit_uid: 'm2PHOpQMv6A',
-      COCUID: '',
-      AOCUID: '',
-      datavalue: ''
-    },
-    {
-      data_element: 'NprBzmM2v3q',
-      period: '201906',
-      org_unit_uid: 'IADWoI6C5v6',
-      COCUID: '',
-      AOCUID: '',
-      datavalue: '0'
-    },
-    {
-      data_element: 'VpBmMqoI3hr',
-      period: '202009',
-      org_unit_uid: 'GGTLdoDHbrR',
-      COCUID: '',
-      AOCUID: '',
-      datavalue: ''
-    },
-    {
-      data_element: 'ZBqvmZXmKQh',
-      period: '202202',
-      org_unit_uid: 'NblWOTY56TS',
-      COCUID: '',
-      AOCUID: '',
-      datavalue: ''
-    }
-  ]
-
-  module.export ={
-    rawData
+  {
+    data_element: 'qi2gcfjfkgk',
+    period: '206906',
+    org_unit_uid: 'tfjhkhgn',
+    COCUID: '',
+    AOCUID: '',
+    datavalue: null
+  },
+  {
+    data_element: 'qi2gcfjfkgk',
+    period: '206906',
+    org_unit_uid: 'tfjhkhgn',
+    COCUID: '',
+    AOCUID: '',
+    datavalue: null
+  },
+  {
+    data_element: 'qi2gcfjfkgk',
+    period: '206906',
+    org_unit_uid: 'tfjhkhgn',
+    COCUID: '',
+    AOCUID: '',
+    datavalue: null
+  },
+  {
+    data_element: 'qi2gcfjfkgk',
+    period: '206906',
+    org_unit_uid: 'tfjhkhgn',
+    COCUID: '',
+    AOCUID: '',
+    datavalue: null
+  },
+  {
+    data_element: 'qi2gcfjfkgk',
+    period: '206906',
+    org_unit_uid: 'tfjhkhgn',
+    COCUID: '',
+    AOCUID: '',
+    datavalue: null
   }
+]
+module.exports ={
+rawData
+}
